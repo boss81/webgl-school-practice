63f89913f0ebc6d74d8b55d1a78f5181c9946837
boss81 <>
2020-07-05 22:22:37 +0900

    update: ボイラープレートを作成

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-document.querySelectorAll(".header-menu, .header-links").forEach(target => {
-  target.addEventListener("click", () => {
-    document.querySelector(".header").classList.toggle("is-header-active");
-  });
-});
